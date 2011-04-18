fb6a9be7ed9184710a08e45a984841afb2810e19
Ben <ben@benrhughes.com>
2011-04-18 19:59:59 +1000

    Adding a swag of missing files that I'd somehow missed

---
+++ Client/FilterDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for FilterDialog.xaml
+    /// </summary>
+    public partial class FilterDialog : Window
+    {
+        public string FilterText
+        {
+            get { return tbFilter.Text; }
+            set { tbFilter.Text = value; }
+        }
+
+        public FilterDialog()
+        {
+            InitializeComponent();
+            tbFilter.Focus();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void tbFilter_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.DialogResult = true;
+        }
+    }
+}
---
+++ Client/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hughesoft")]
+[assembly: AssemblyProduct("todotxt.net")]
+[assembly: AssemblyCopyright("Copyright © Ben Hughes 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
