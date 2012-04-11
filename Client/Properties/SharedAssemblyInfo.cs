5cecc3913ca659a135e452e32bff14fe34cf5b70
Ben <ben@benrhughes.com>
2012-04-11 12:15:43 +1000

    Updated version to 1.2.35.0

--- Client/Properties/SharedAssemblyInfo.cs
+++ Client/Properties/SharedAssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.11.0")]
-[assembly: AssemblyFileVersion("1.2.11.0")]
+[assembly: AssemblyVersion("1.2.35.0")]
+[assembly: AssemblyFileVersion("1.2.35.0")]
