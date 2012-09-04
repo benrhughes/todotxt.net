ad53d5d41ae637fea6b147e21fbde68570e82921
Andrew Stampor <arphahat@gmail.com>
2012-09-04 02:01:34 -0400

    Added font selection for task list -- Issue #12
    Added support for font family, style and weight, size and color.
    Currently limited to 50, this should ideally be user configurable.
    However, the situtations where something larger would be useful is
    limited.
    
    Originally tried using the FontDialog from Forms, but it was too
    cumbersome moving between the two font methodologies.  Instead, I found
    a WPF font picker at CodeProject: http://goo.gl/vmujX .   The CPOL
    license allows it to be safely included in the solution.
    
    Font objects are not serializable, so I created six settings instead of
    just one and assemble into a font.

---
+++ ColorFont/ColorFontDialog.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorFont
+{
+    /// <summary>
+    /// Interaction logic for ColorFontDialog.xaml
+    /// </summary>
+public partial class ColorFontDialog : Window
+{
+    private FontInfo selectedFont;
+
+    public ColorFontDialog()
+    {
+        this.selectedFont = null; // Default
+        InitializeComponent();
+    }
+
+    public FontInfo Font
+    {
+        get
+        {
+            return this.selectedFont;
+        }
+
+        set
+        {
+            FontInfo fi = value;
+            this.selectedFont = fi;
+        }
+    }
+
+    private void SyncFontName()
+    {
+        string fontFamilyName = this.selectedFont.Family.Source;
+        int idx = 0;
+        foreach (var item in this.colorFontChooser.lstFamily.Items)
+        {
+            string itemName = item.ToString();
+            if (fontFamilyName == itemName)
+            {
+                break;
+            }
+            idx++;
+        }
+        this.colorFontChooser.lstFamily.SelectedIndex = idx;
+        this.colorFontChooser.lstFamily.ScrollIntoView(this.colorFontChooser.lstFamily.Items[idx]);
+    }
+
+    private void SyncFontSize()
+    {
+        double fontSize = this.selectedFont.Size;
+        this.colorFontChooser.fontSizeSlider.Value = fontSize;
+    }
+
+    private void SyncFontColor()
+    {
+        int colorIdx = AvailableColors.GetFontColorIndex(this.Font.Color);
+        this.colorFontChooser.colorPicker.superCombo.SelectedIndex = colorIdx;
+        // The following does not work. Why???
+        // this.colorFontChooser.colorPicker.superCombo.SelectedValue = this.Font.Color;
+        this.colorFontChooser.colorPicker.superCombo.BringIntoView();
+    }
+
+    private void SyncFontTypeface()
+    {
+        string fontTypeFaceSb = FontInfo.TypefaceToString(this.selectedFont.Typeface);
+        int idx = 0;
+        foreach (var item in this.colorFontChooser.lstTypefaces.Items)
+        {
+            FamilyTypeface face = item as FamilyTypeface;
+            if (fontTypeFaceSb == FontInfo.TypefaceToString(face))
+            {
+                break;
+            }
+            idx++;
+        }
+        this.colorFontChooser.lstTypefaces.SelectedIndex = idx;
+    }
+
+    private void btnOk_Click(object sender, RoutedEventArgs e)
+    {
+        this.Font = this.colorFontChooser.SelectedFont;
+        this.DialogResult = true;
+    }
+
+    private void Window_Loaded_1(object sender, RoutedEventArgs e)
+    {
+        this.SyncFontColor();
+        this.SyncFontName();
+        this.SyncFontSize();
+        this.SyncFontTypeface();
+    }
+}
+}
---
+++ ColorFont/FontColor.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Media;
+
+namespace ColorFont
+{
+    public class FontColor
+    {
+        public string Name { get; set; }
+        public SolidColorBrush Brush { get; set; }
+
+        public FontColor(string name, SolidColorBrush brush)
+        {
+            Name = name;
+            Brush = brush;
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            FontColor p = obj as FontColor;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            return (this.Name == p.Name) && (this.Brush.Equals(p.Brush));
+        }
+
+        public bool Equals(FontColor p)
+        {
+            if ((object)p == null)
+            {
+                return false;
+            }
+
+            return (this.Name == p.Name) && (this.Brush.Equals(p.Brush));
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "FontColor [Color=" + this.Name + ", " + this.Brush.ToString() + "]";
+        }
+    }
+}
