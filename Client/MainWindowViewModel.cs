d3f666a06fc63da47434b76e4ca6ad31d0884c82
Lex Mosolov <lex.mosolov@gmail.com>
2014-04-25 01:56:11 +0400

    Listbox loses focus when last item has been deleted

--- Client/MainWindowViewModel.cs
+++ Client/MainWindowViewModel.cs
@@ -317,8 +317,15 @@ namespace Client
             {
                 var match = _taskList.Tasks.FirstOrDefault(x => x.Body.Equals(selected.Body, StringComparison.InvariantCultureIgnoreCase));
                 if (match == null)
-                {
-                    _window.lbTasks.SelectedIndex = selectedIndex;
+                {            
+                    if (selectedIndex == _window.lbTasks.Items.Count)
+                    {
+                        _window.lbTasks.SelectedIndex = selectedIndex - 1;
+                    }
+                    else
+                    {
+                        _window.lbTasks.SelectedIndex = selectedIndex;
+                    }
                 }
                 else
                 {
