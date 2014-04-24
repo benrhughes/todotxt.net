1f539904b3948fb23dcc272fa3e59865839d4079
Ben <ben@benrhughes.com>
2014-04-24 21:44:52 +1000

    Preparing release 2.4.0

--- Client/Properties/SharedAssemblyInfo.cs
+++ Client/Properties/SharedAssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
