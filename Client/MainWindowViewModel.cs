benrhughes/todotxt.net

commit 645f572055ad78d1a76129252e93fa5bd4993146

Closes Issue #249: Bug: When there are duplicate tasks and one is selected, all get selected after sorting the task list.

Client/MainWindowViewModel.cs
@@ -141,8 +141,10 @@ namespace Client
             for (int i = 0; i < _window.lbTasks.Items.Count; i++)
             {
                 Task listBoxItemTask = _window.lbTasks.Items[i] as Task;
-                foreach (var task in _selectedTasks)
+                int j = 0;
+                while (j < _selectedTasks.Count)
                 {
+                    Task task = _selectedTasks[j];
                     if (listBoxItemTask.Raw.Equals(task.Raw))
                     {
                         _window.lbTasks.SelectedItems.Add(_window.lbTasks.Items[i]);
@@ -153,6 +155,12 @@ namespace Client
                         {
                             SelectTaskByIndex(i);
                         }
+
+                        _selectedTasks.RemoveAt(j);
+                    }
+                    else
+                    {
+                        j++;
                     }
                 }
             }

commit eb809acff5f691334e308814f35125886f0d1d98

This patch fixes a regression introduced in commit 645f572 (When duplicate tasks are selected, only one of those tasks retains selection after a sort, filter, or other operation). Closes Issue #249.

Client/MainWindowViewModel.cs
@@ -157,6 +157,7 @@ namespace Client
                         }
 
                         _selectedTasks.RemoveAt(j);
+                        break;
                     }
                     else
                     {