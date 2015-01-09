benrhughes/todotxt.net

commit b6e00dc2ceac63539077d92583d3857fdc157bd0

Fixes bug in IntellisenseTextBox.cs, in which the Escape key, when pressed when the Intellisense popup listbox has focus, would not only cancel Intellisense, but also clear the text box in MainWindow.xaml. Related to Issue #66.

Client/Controls/IntellisenseTextBox.cs
@@ -139,6 +139,7 @@ namespace Client
                     HideIntellisensePopup();
                     this.CaretIndex = this.Text.Length;
                     this.Focus();
+                    e.Handled = true;
                     break;
             }
         }

commit 223893d05b42a3aa100fb2c1187194611ef8884e

Fixes bug introduced in b6e00dc that caused exceptions in IntellisenseTextBox in the Filter dialog. Changes how IntellisenseTextBox TaskList property is set and passed from MainWindowViewModel to FilterDialog.xaml.

Client/Controls/FilterDialog.xaml
@@ -26,13 +26,13 @@
             </TextBlock>
         </Label>
         <Label Content="Currently active filter:" />
-        <local:IntellisenseTextBox x:Name="tbFilter" PreviewKeyUp="tbFilter_PreviewKeyUp" />
+        <local:IntellisenseTextBox x:Name="tbFilter" PreviewKeyUp="tbFilter_PreviewKeyUp" TaskList="{Binding TaskList}" />
         <Label Content="Preset filter #1:" />
-        <local:IntellisenseTextBox x:Name="tbFilterPreset1" PreviewKeyUp="tbFilter_PreviewKeyUp" />
+        <local:IntellisenseTextBox x:Name="tbFilterPreset1" PreviewKeyUp="tbFilter_PreviewKeyUp" TaskList="{Binding TaskList}" />
         <Label Content="Preset filter #2:" />
-        <local:IntellisenseTextBox x:Name="tbFilterPreset2" PreviewKeyUp="tbFilter_PreviewKeyUp" />
+        <local:IntellisenseTextBox x:Name="tbFilterPreset2" PreviewKeyUp="tbFilter_PreviewKeyUp" TaskList="{Binding TaskList}" />
         <Label Content="Preset filter #3:" />
-        <local:IntellisenseTextBox x:Name="tbFilterPreset3" PreviewKeyUp="tbFilter_PreviewKeyUp" />
+        <local:IntellisenseTextBox x:Name="tbFilterPreset3" PreviewKeyUp="tbFilter_PreviewKeyUp" TaskList="{Binding TaskList}" />
         <Grid Margin="0,12,0,0">
             <Grid.RowDefinitions>
                 <RowDefinition Height="23"/>

Client/Controls/FilterDialog.xaml.cs
@@ -9,6 +9,7 @@ namespace Client
     public partial class FilterDialog : Window
     {
 		private bool _prevKeyWasEnter;
+        private MainWindowViewModel _parentWindowViewModel;
 
         public string FilterText
         {
@@ -33,11 +34,18 @@ namespace Client
             get { return tbFilterPreset3.Text; }
             set { tbFilterPreset3.Text = value; tbFilterPreset3.CaretIndex = tbFilterPreset3.Text.Length; }
         }
-        
+
+        public FilterDialog(MainWindowViewModel parentWindowViewModel) : this()
+        {
+            _parentWindowViewModel = parentWindowViewModel;
+            this.DataContext = _parentWindowViewModel;
+        }
+
         public FilterDialog()
         {
             InitializeComponent();
             tbFilter.Focus();
+            _parentWindowViewModel = null;
         }
 
         private void OK_Click(object sender, RoutedEventArgs e)

Client/Controls/IntellisenseTextBox.cs
@@ -21,20 +21,19 @@ namespace Client
         private Popup IntellisensePopup { get; set; }
         private ListBox IntellisenseList { get; set; }
         private int IntelliPos { get; set; } // used to position the Intellisense popup
-
-	    private TaskList TaskList
-	    {
-		    get { return MainWindowViewModel.TaskList; }
-	    }
-
-	    public MainWindowViewModel MainWindowViewModel
-	    {
-		    get { return (MainWindowViewModel) GetValue(MainWindowViewModelProperty); }
-		    set { SetValue(MainWindowViewModelProperty, value); }
-	    }
-
-	    public static readonly DependencyProperty MainWindowViewModelProperty =
-		DependencyProperty.Register("MainWindowViewModel", typeof(MainWindowViewModel), typeof(IntellisenseTextBox), new UIPropertyMetadata(null));
+        public TaskList TaskList
+        {
+            get
+            {
+                return (TaskList)GetValue(TaskListProperty);
+            }
+            set
+            {
+                SetValue(TaskListProperty, value);
+            }
+        }
+        public static readonly DependencyProperty TaskListProperty =
+            DependencyProperty.Register("TaskList", typeof(TaskList), typeof(IntellisenseTextBox), new UIPropertyMetadata(null));
 
         #endregion
 

Client/Controls/MainWindow.xaml
@@ -216,7 +216,7 @@
         </Menu>
         <local:IntellisenseTextBox Height="23" Margin="0,23,0,0" x:Name="taskText" AcceptsReturn="False" Cursor="IBeam" VerticalAlignment="Top"
                                    KeyUp="taskText_PreviewKeyUp" 
-								   MainWindowViewModel ="{Binding DataContext, ElementName=ToDoMainWindow}"/>
+                                   TaskList="{Binding TaskList}" />
         <ListBox ItemsSource="{Binding Tasks}" IsSynchronizedWithCurrentItem="True" SelectedItem="{Binding SelectedItem}" Name="lbTasks" Margin="0,46,0,0" SelectedIndex="0" IsTextSearchEnabled="False" AlternationCount="2" BorderBrush="{x:Null}" SelectionMode="Extended">
             <ListBox.CommandBindings>
                 <CommandBinding Command="ApplicationCommands.Copy" CanExecute="WhenTasksSelectedCanExecute" Executed="CopyTasksExecuted" />

Client/MainWindowViewModel.cs
@@ -362,7 +362,7 @@ namespace Client
 
         public void ShowFilterDialog()
         {
-            var f = new FilterDialog();
+            var f = new FilterDialog(this);
             f.Owner = _window;
 
             f.FilterText = User.Default.FilterText;