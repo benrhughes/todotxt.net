254ae5a32ea151818a029a628061b69f05a7669e
Ben Hughes <ben@benrhughes.com>
2016-08-29 21:07:27 +1000

    Fix build/installer issue caused by 'using static' which MS

--- Client/Converters/SortTypeToDescriptionConvertor.cs
+++ Client/Converters/SortTypeToDescriptionConvertor.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Windows.Data;
-using static Client.Utilities.EnumExtensions;
+using Client.Utilities;
 
 namespace Client.Converters
 {
