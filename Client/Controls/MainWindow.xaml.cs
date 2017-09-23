c226343cca9cd4eff5adfa2fd81fa557244c6af9
benrhughes <ben@benrhughes.com>
2017-09-23 20:24:30 +1000

    Update mshtml reference to use nuget as it doesn't seem to be in the GAC for Windows 10

--- Client/Controls/MainWindow.xaml.cs
+++ Client/Controls/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using mshtml;
 
 namespace Client
 {
--- Installer/Create-Installer.bat
+++ Installer/Create-Installer.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" Build.xml
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Build.xml
